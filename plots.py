a0e484695e379d325c5c585655f38ba03184345a
Humna <humna.awan@rutgers.edu>
2018-02-04 19:10:28 -0800

    Added function to plot multiple corrs to a plot and save.

--- plots.py
+++ plots.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 import numpy as np
+import os
 
 def plotDiagnostics(dataDF, bands= ['g', 'r', 'i', 'z', 'y']): # assume cleaned up data
     ##########################################
@@ -68,3 +70,51 @@ def plot_wtheta(theta, wtheta, wtheta_sig, title= None):
     ax.tick_params(axis='x', labelsize=fontsize)
     ax.tick_params(axis='y', labelsize=fontsize)
     plt.show()
+    
+def plot_wthetas(theta_dict, wtheta_dict, wtheta_sig_dict, title= None,
+                 savePlot= False, outDir= None, filename= None):
+    # theta assumed to be in degrees
+    if title is None: title= ''
+
+    colors= ['b', 'r', 'g', 'k', 'm', 'c']
+    
+    plt.clf()
+    fig, ax= plt.subplots(1,1)
+    for i, tag in enumerate(theta_dict):
+        ax.plot(theta_dict[tag], np.zeros(len(theta_dict[tag]))+1, color= 'k', lw=2, linestyle= ':')
+        ax.plot(theta_dict[tag], np.zeros(len(theta_dict[tag])), color= 'k', lw=2, linestyle= ':')
+        ax.plot(theta_dict[tag], np.zeros(len(theta_dict[tag]))-1, color= 'k', lw=2, linestyle= ':')
+
+        if (i>len(colors)):
+            style= '.-'
+            i= i%len(colors)
+        else:
+            style= 'o-'
+        ax.plot(theta_dict[tag], wtheta_dict[tag], style, lw= 2, color= colors[i], label= tag)
+        ax.errorbar(theta_dict[tag], wtheta_dict[tag], yerr= wtheta_sig_dict[tag], color=colors[i])
+
+    fontsize= 12
+    ax.legend(fontsize= fontsize)
+    ax.set_title(title, fontsize= fontsize)
+    ax.set_xlabel(r'$\theta$ (Degrees)', fontsize= fontsize+2)
+    ax.set_ylabel(r'$w(\theta)$', fontsize= fontsize+2)
+
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.tick_params(axis='x', labelsize=fontsize)
+    ax.tick_params(axis='y', labelsize=fontsize)
+    
+    if savePlot:
+        if outDir is None:
+            raise ValueError('Need outDir if want to save plot.')
+        if filename is None:
+            raise ValueError('Need filename if want to save plot.')
+            
+        current= os.getcwd()
+        os.chdir(outDir)
+        plt.savefig(filename,bbox_inches='tight')
+        plt.close('all')
+        os.chdir(current)
+        print('Saved ', filename)
+    else:
+        plt.show()
