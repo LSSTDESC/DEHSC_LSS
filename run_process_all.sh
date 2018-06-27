c4af5a01ac614f43c776ea96b4c108e7ff8c585f
David Alonso <dam.phys@gmail.com>
2018-06-27 12:02:31 +0100

    added to pipeline

--- run_process_all.sh
+++ run_process_all.sh
@@ -2,18 +2,27 @@
 
 predir_out=/global/cscratch1/sd/damonge/HSC
 
+
 #First clean up the metadata
 for table in WIDE DEEP UDEEP
 do
     python process_metadata.py --input-file ${predir_out}/HSC_${table}_frames.fits --output-file ${predir_out}/HSC_processed/HSC_${table}_frames_proc.fits
 done
 
+#Now clean up the WIDE fields
 for field in WIDE_AEGIS WIDE_GAMA09H WIDE_GAMA15H WIDE_GAMA15H WIDE_HECTOMAP WIDE_VVDS WIDE_WIDE12H WIDE_XMMLSS
 do
     dirname=${predir_out}/HSC_processed/${field}
     mkdir -p ${dirname}
     #python process.py --input-field ${field} --resolution 0.01 --field-padding 0.1 --output-prefix ${dirname}/${field} --save-systematics --save-masks --save-depth-maps --gen-plots --min-snr 10.0 --depth-cut 24.5
     python process.py --input-field ${field} --resolution 0.01 --field-padding 0.1 --output-prefix ${dirname}/${field} --save-systematics --save-masks --save-depth-maps --min-snr 10.0 --depth-cut 24.5
+done
+
+#Compute per-frame systematic maps
+for field in WIDE_AEGIS WIDE_GAMA09H WIDE_GAMA15H WIDE_GAMA15H WIDE_HECTOMAP WIDE_VVDS WIDE_WIDE12H WIDE_XMMLSS
+do
+    dirname=${predir_out}/HSC_processed/${field}
+    python map_obscond.py --input-frames ${predir_out}/HSC_processed/HSC_WIDE_frames_proc.fits --map-sample ${dirname}/${field}_MaskedFraction.fits --output-prefix ${dirname}/${field}
 done
 
 #So far we've only looked at the WIDE fields
