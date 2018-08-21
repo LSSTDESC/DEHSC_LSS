440663b39256c0847292547afbe4ac76ea2f7281
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2018-08-21 15:27:14 -0400

    Added a PIT plotter to pdf_reader.py and added columns to hsc_reader class in photoz.py

--- photoz_scripts/pdf_reader.py
+++ photoz_scripts/pdf_reader.py
@@ -5,7 +5,10 @@ import pandas as pd
 from glob import glob
 from astropy.io import fits
 from tqdm import tqdm
+import photoz
+import os
 
+hsc_cat = photoz.hsc_cat
 
 
 class reader:
@@ -39,5 +42,62 @@ class reader:
 
 
 
-def plot_PIT():
-    pass
+def get_PIT(pdfreader, cachefile = './data/cache/PIT_cache.dat'):
+
+    if os.path.isfile(cachefile):
+        hsc_indices, match_indices, integrals = np.loadtxt(cachefile, unpack = True)
+
+    else:
+
+        hsc_indices = [] # The HSC objects with matches in the pdf indices
+        match_indices = [] # The matching pdf indices
+
+        for x in tqdm(xrange(len(hsc_cat.ephor_mc))):
+
+            index = np.where(hsc_cat.object_id[x] == pdfreader.object_id)[0]
+
+            if len(index) != 0:
+                index = index[0]
+                hsc_indices.append(x)
+
+                match_indices.append(index)
+
+        mod_bins = [pdfreader.bins,] * len(hsc_indices)
+        mod_pdfs = []
+
+        for x in tqdm(xrange(len(hsc_indices))):
+
+            mod_bins[x] = mod_bins[x][mod_bins[x] < hsc_cat.ephor_median[hsc_indices[x]]]
+            mod_pdfs.append(pdfreader.pdf[index][:len(mod_bins[x])])
+
+        mod_pdfs = np.array(mod_pdfs)
+        match_indices = np.array(match_indices)
+        mod_bins = np.array(mod_bins)
+
+        integrals = []
+
+        # Calculate the cumulative integral from the lower bound to the estimated redshift
+
+        # integrals = np.trapz(mod_pdfs, x = mod_bins)/np.trapz(pdfreader.pdf[match_indices], x = pdfreader.bins[match_indices])
+
+        for x in tqdm(xrange(len(mod_bins))):
+            integrals.append(np.trapz(mod_pdfs[x], x = mod_bins[x])/np.trapz(pdfreader.pdf[match_indices[x]], x = pdfreader.bins))
+
+        integrals = np.array(integrals)
+
+        tabledata = np.array((hsc_indices, match_indices, integrals)).T
+        os.mkdir('./data/cache/')
+        np.savetxt(cachefile, tabledata, header = 'hsc_index pdf_index integrals \n', fmt = '  %8i  %8i  %8f')
+
+    return hsc_indices, match_indices, integrals
+
+
+def plot_PIT(pdfreader):
+
+    hsc_indices, match_indices, integrals = get_PIT(pdfreader)
+
+    fig = plt.figure(figsize = (8,8))
+    sp = fig.add_subplot(111)
+
+    sp.hist(integrals, bins = 50, normed = True, histtype = 'step', color = 'k')
+    sp.plot([0,1],[1,1], color = 'r')
--- photoz_scripts/photoz.py
+++ photoz_scripts/photoz.py
@@ -56,11 +56,12 @@ class cosmos_reader:
 
 class hsc_reader:
 
-    def __init__(self, inputfp = './hscdeep_zcosmos.csv'):
+    def __init__(self, inputfp = './data/hscdeep_zcosmos.csv'):
 
         (self.object_id, self.specz_id, self.ra, self.dec, 
             self.mag_g, self.mag_r, self.mag_i, self.mag_z, self.mag_y, 
-            self.z_spec, self.flag_zcosmos, self.flag_cosmos_fmos) = pd.read_csv(inputfp).values.T
+            self.z_spec, self.ephor_mc, self.ephor_median, self.ephor_best,
+            self.flag_zcosmos, self.flag_cosmos_fmos) = pd.read_csv(inputfp).values.T
 
 
 hsc_cat = hsc_reader()
