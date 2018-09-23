68e140b606172c322a086ead49ffa25375b0f107
Humna <humna.awan@rutgers.edu>
2018-09-23 10:30:27 -0700

    Moved the SN calculation code out of the notebook.

---
+++ photoz_binning/get_sn_photoz_bins.py
@@ -0,0 +1,130 @@
+# The goal here to calculate the SN as a function of Nbin.
+#
+# We read in the matched PDFs so that we have a PDF for each of the galaxy in the processed catalog. Then, we find bin edges for different Nbin (number of photo-z bins), using the photo-z for each galaxy; these bins are selected s.t. each bin has roughly the same number of galaxies.
+#
+# Using the point estimates and the PDFs, we calculate dn/dz for each bin; see more in the `get_dn_dz` method. Finally, we use CCL to calculate the signal, the power spectra for each bin pair (i,j), while the point estimates are used to estimate the number density of galaxies in each bin to estimate the shot noise. The final output cell plots the S/N as a function of Nbin.
+#
+# A few specifics: we work with `AEGIS` field and `ephor_ab` outputs, for $2<\ell<2000$; `pz_mc_eab` are used for photo-z point estimates and Nbin=1-6. Also, for now, the area of the field is assumed to be 108/7 ~ 15.4 deg2.
+#
+#
+##############################################################################################################################
+import numpy as np
+import pandas as pd
+import os
+from astropy.table import Table
+import time
+from astropy.io import fits
+from photoz_bin_sn_utils import get_bin_edges, calc_sn
+import matplotlib.pyplot as plt
+
+##############################################################################################################################
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option('--cat_data_main_path', dest='data_main_path',
+                  help='Path to the processed data.',
+                  default='/global/cscratch1/sd/damonge/HSC/HSC_processed')
+parser.add_option('--pdfs_main_path', dest='pdfs_main_path',
+                  help='Path to the matched PDFs.',
+                  default='/global/cscratch1/sd/awan/hsc_matched_pdfs_old/')
+parser.add_option('--fields', dest='fields',
+                  help='List of fields to consider',
+                  default='wide_aegis, wide_gama09h, wide_gama15h, wide_hectomap, wide_vvds, \
+                           wide_wide12h, wide_xmmlss, deep_cosmos, deep_elaisn1, deep_xmmlss, deep_deep23')
+parser.add_option('--PZalg', dest='PZalg',
+                  help='List of PZ algorithms to consider',
+                  default='ephor, ephor_ab, demp, frankenz')
+parser.add_option('--outDir', dest='outDir',
+                  help='Path to the folder where the plots will be saved.',
+                  default='/global/cscratch1/sd/awan/lsst_output/hsc_output/')
+
+##############################################################################################################################
+startTime = time.time()
+(options, args) = parser.parse_args()
+print('\nOptions: %s'%options)
+
+# read in the inputs
+datapath = options.data_main_path
+pdfs_path = options.pdfs_main_path
+fields = options.fields
+PZalg = options.PZalg
+outDir = options.outDir
+
+# format the fields
+fields = [f.strip() for f in list(fields.split(','))]
+PZalg = [f.strip() for f in list(PZalg.split(','))]
+
+# set up some things
+z_phot_key = 'pz_mc_eab'
+ell = np.arange(2, 2000)
+area_in_deg = 108./7. #  for each field. sq deg: based on https://arxiv.org/pdf/1702.08449.pdf
+
+##############################################################################################################################
+# run over all field
+for field in fields:
+    print('\n--------------------------------------------------------------------')
+    print('\nStarting with %s'%field)
+    # ------------------------------------------------------------------------------------------------------------------------
+    # read in the cleaned catalog.
+    for j, folder in enumerate([f for f in os.listdir(datapath) \
+                                if not f.__contains__('.fits') and f.__contains__(field.upper())]):
+        # read only the Catalog fits file for the field
+        for i, cat_file in enumerate([f for f in os.listdir('%s/%s'%(datapath, folder)) if f.__contains__('Catalog')]):
+            if i>0 : raise ValueError('Something is wrong. Have more than one Catalog file in %s/%s'%(datapath, folder))
+
+            print('\nReading in %s'%cat_file)
+            dat = Table.read('%s/%s/%s'%(datapath, folder, cat_file), format='fits')
+        if j==0:
+            hscdata = dat.to_pandas()
+            print('Data read in. Shape: %s'%(np.shape(hscdata),))
+        else:
+            raise ValueError('Something is not right. Have more than one folder for %s: %s'%(field, folder))
+
+    # ------------------------------------------------------------------------------------------------------------------------
+    for alg in PZalg:
+        print('\nStarting with %s'%alg)
+        filename= 'matched_pdfs_ids_bins_wide_%s_%s.fits'%(field, alg)
+        print('Reading in %s'%filename)
+        hdul = fits.open('%s/%s'%(pdfs_path, filename))
+
+        pdfs = np.array(Table(hdul[1].data))
+        pdfs = np.array(pd.DataFrame(pdfs))  # somehow np.array isnt enough to make the table come out nice
+
+        ids = np.array(Table(hdul[2].data)['col0'])
+        bins = np.array(Table(hdul[3].data)['col0'])
+
+        # stack the pdfs to estimate N(z)
+        n_z = np.sum(pdfs, axis=0)
+
+        # --------------------------------------------------------------
+        ### Find the bin edges to consider for different number of bins
+        z_phots = []
+        n_bin_list = []
+        for i in range(1,7):
+            n_bin_list.append(i)
+            print('----------------------------------\nnbin=%s'%i)
+            out = get_bin_edges(nbin=i, hsc_z_phot=hscdata[z_phot_key], z_bins=bins)
+            z_phots.append(out)
+
+        # --------------------------------------------------------------
+        ### Run CCL and calculate S/N for each Nbin
+        sn = np.zeros(len(z_phots))
+        for i, z_phot in enumerate(z_phots):
+            sn[i] = calc_sn(z_phot=z_phot, z_bins=bins, hsc_z_phot=hscdata[z_phot_key], hsc_ids=hscdata['object_id'],
+                            matched_pdf_ids=ids.copy(), matched_pdfs=pdfs.copy(), n_z=n_z, ell=ell, area_in_deg=area_in_deg,
+                            plot_cls=False)
+
+        # plot SN as a function of Nbin
+        plt.clf()
+        plt.plot(n_bin_list, sn, 'o-')
+        plt.xlabel('Nbin')
+        plt.ylabel('S/N')
+        plt.gca().ticklabel_format(style='sci', scilimits=(-3,4),axis='y')
+        plt.xticks(n_bin_list, n_bin_list)#, rotation=70)
+        plt.show()
+
+    # ------------------------------------------------------------------------------------------------------------------------
+    time_taken = time.time()-startTime
+    if (time_taken>60.):
+        print('\nTime taken %.2f min '%(time_taken/60.) )
+    else:
+        print('\nTime taken %.2f sec '%time_taken)
---
+++ photoz_binning/photoz_bin_sn_utils.py
@@ -0,0 +1,191 @@
+import numpy as np
+import pyccl as ccl
+import matplotlib.pyplot as plt
+
+__all__ = ['get_dn_dz', 'get_bins_ends', 'get_bin_edges',
+          'get_surface_number_density', 'calc_sn']
+
+# --------------------------------------------------------------------------------------------------------
+def get_dn_dz(z_bins, hsc_z_phot, hsc_ids, matched_pdf_ids, matched_pdfs):
+    # to get dn/dz, we first find all the galaxies whose z_phot are in a given bin
+    # then dn/dz is the sum of the pdfs of these galaxies
+    print('Running get_dn_dz ... ')
+    dndz = {}
+    for i in range(len(z_bins)-1):
+        zmin, zmax = z_bins[i], z_bins[i+1]
+        bin_key = '%s<z<%s'%(zmin, zmax)
+        dndz[bin_key] = []
+        ind = np.where((hsc_z_phot >= zmin) & (hsc_z_phot < zmax))[0]
+        print('Considering pdfs of %s objects for %s'%(len(ind), bin_key))
+
+        for j, ID in enumerate(hsc_ids[ind]):
+            row = np.where(matched_pdf_ids==ID)[0][0]
+            if j==0:
+                dndz[bin_key] = matched_pdfs[row]
+            else:
+                dndz[bin_key] += matched_pdfs[row]
+    return dndz
+
+# --------------------------------------------------------------------------------------------------------
+def get_bins_ends(z_bin_array):
+    # get the labels and bin edges based on the input z-bin array
+    bin_keys, bin_ends = [], {}
+    for i in range(len(z_bin_array)-1):
+        zmin, zmax = z_bin_array[i], z_bin_array[i+1]
+        bin_key = '%s<z<%s'%(zmin, zmax)
+        bin_keys.append(bin_key)
+        bin_ends[bin_key] = [zmin, zmax]
+    return np.array(bin_keys), bin_ends
+
+# --------------------------------------------------------------------------------------------------------
+def get_bin_edges(nbin, hsc_z_phot, z_bins):
+    # decide on bins edges: we look for bin edges that have roughly the same number of galaxies
+    zmin, zmax = 0.15, 1.5   # these are fixed since this is the range where z_phot is reliable
+    obj_thres = 3000    # the bins would have galaxies within this threshold of each other
+    if nbin==0:
+        print('nbin must be greater than one.')
+        return
+    else:
+        n_obj = len(np.where((hsc_z_phot > zmin) & (hsc_z_phot < zmax))[0])
+        if nbin==1:
+            print('%s objects in %s<z<%s'%(n_obj, zmin, zmax))
+            bin_ends = np.array([zmin, zmax])
+        else:
+            # want the same number of objects in the bins
+            wanted_n_obj_in_bin = n_obj/nbin
+            print('Looking for bins with about %s galaxies each ... '%wanted_n_obj_in_bin)
+
+            bin_ends = np.zeros(nbin+1)
+            bin_ends[0] = zmin
+            bin_ends[-1] = zmax
+
+            for i in range(nbin-1):
+                print('\nLooking for %sth bin egde'%(i+1))
+                n_obj = 0
+                j = 0
+                good_edges = [f for f in z_bins if f>bin_ends[i]]
+
+                while abs(n_obj-wanted_n_obj_in_bin)>obj_thres and j<len(good_edges):
+                    z_edge = good_edges[j]
+                    n_obj = len(np.where((hsc_z_phot > bin_ends[i]) & (hsc_z_phot < z_edge))[0])
+                    #print('## %s objects in %s<z<%s'%(n_obj, bin_ends[i], z_edge))
+                    j += 1
+                if abs(n_obj-wanted_n_obj_in_bin)>obj_thres:
+                    print('Something is wrong.')
+
+                if (z_edge==bin_ends[-1]):
+                    print('Need to change the threshold on the number of galaxies: %s currently'%(obj_thres))
+
+                bin_ends[i+1] = float('%.2f'%z_edge)
+
+            print('\nFinal:')
+            for i in range(nbin):
+                n_obj = len(np.where((hsc_z_phot > bin_ends[i]) & (hsc_z_phot < bin_ends[i+1]))[0])
+                print('%s objects in %s<z<%s'%(n_obj, bin_ends[i], bin_ends[i+1]))
+
+        return bin_ends
+# --------------------------------------------------------------------------------------------------------
+def get_surface_number_density(n_objs, area_in_deg):  # in 1/Sr
+    area_in_sr = area_in_deg*(np.pi/180.)**2
+    return n_objs/area_in_sr
+
+# --------------------------------------------------------------------------------------------------------
+def calc_sn(z_phot, z_bins, hsc_z_phot, hsc_ids, matched_pdf_ids, matched_pdfs, n_z, ell, area_in_deg, plot_cls=True):
+    print('-----------------------------------------\nRunning calc_sn ... ')
+
+    z_all = [min(z_bins)] + list(z_phot) + [max(z_bins)]   # cover the entire range, even outside the target bins
+
+    target_bins, target_bin_ends = get_bins_ends(z_bin_array=z_phot)
+    all_bins, all_bin_ends = get_bins_ends(z_bin_array=z_all)
+
+    print('Target bins: %s'%(target_bins))
+    print('All bins: %s\n'%(all_bins))
+
+    dn_dz = get_dn_dz(z_bins=z_all, hsc_z_phot=hsc_z_phot, hsc_ids=hsc_ids,
+                      matched_pdf_ids=matched_pdf_ids, matched_pdfs=matched_pdfs)
+    plt.clf()
+    for key in dn_dz:
+        if key in target_bin_ends:
+            plt.plot(z_bins, dn_dz[key], lw=4, label='** dn_dz: %s'%key)
+            for z_edge in target_bin_ends[key]:
+                plt.plot([z_edge, z_edge], [0,7000], 'k-.', alpha=0.7)
+        else:
+            plt.plot(z_bins, dn_dz[key], lw=4, label='dn_dz: %s'%key)
+    plt.plot(z_bins, n_z, 'k:', label='n_z', alpha=0.7)
+    plt.legend()
+    plt.xlabel('z')
+    plt.show()
+
+    # set up ccl
+    cosmo_fid= ccl.Cosmology(Omega_c=0.25, Omega_b=0.05, h=0.7, sigma8=0.8, n_s=0.96)
+    bias = 1 + 0.84*z_bins
+
+    # calculate the signal matrix
+    print('Calculating the signal matrix ... ')
+    S = np.zeros(shape=(len(ell), len(all_bins), len(all_bins)))
+    for i, bin1_key in enumerate(all_bins):
+        nc_bin1 = ccl.ClTracerNumberCounts(cosmo_fid, has_rsd=False, has_magnification=False,
+                                            n=dn_dz[bin1_key], bias=bias, z=z_bins)
+        for j, bin2_key in enumerate(all_bins):
+            nc_bin2= ccl.ClTracerNumberCounts(cosmo_fid, has_rsd=False, has_magnification=False,
+                                                  n=dn_dz[bin2_key], bias=bias, z=z_bins)
+            S[:, i, j] = ccl.angular_cl(cosmo_fid, nc_bin1, nc_bin2, ell)
+
+    # calculate the noise matrix
+    print('Calculating the noise matrix ... ')
+    N = np.zeros(shape=(len(ell), len(all_bins), len(all_bins)))
+    for i, bin_key in enumerate(all_bins):
+        zmin, zmax = all_bin_ends[bin_key]
+        n_obj = len(np.where((hsc_z_phot > zmin) & (hsc_z_phot < zmax))[0])
+        print('%s objects in %s'%(n_obj, bin_key))
+        num_density = get_surface_number_density(n_objs=n_obj, area_in_deg=area_in_deg)
+        N[:, i, i] = 1./num_density
+
+    ###############################################################################################
+    # plot all cls
+    if plot_cls:
+        for i, bin1_key in enumerate(all_bins):
+            for j, bin2_key in enumerate(all_bins):
+                if i!=j:
+                    plt.plot(ell, S[:, i, j], 'o-', label='%s-%s'%(bin1_key, bin2_key))
+        plt.xlabel('$\ell$')
+        plt.ylabel('Cross C$_\ell$')
+        plt.gca().set_xscale('log')
+        plt.gca().ticklabel_format(style='sci',scilimits=(-3,4),axis='y')
+        plt.legend(bbox_to_anchor=(1,.5))
+        plt.show()
+
+        # plot all cls
+        for i, bin1_key in enumerate(all_bins):
+            for j, bin2_key in enumerate(all_bins):
+                if i==j:
+                    plt.plot(ell, S[:, i, j], 'o-', label='%s-%s'%(bin1_key, bin2_key))
+        plt.xlabel('$\ell$')
+        plt.ylabel('Auto C$_\ell$')
+        plt.gca().ticklabel_format(style='sci',scilimits=(-3,4),axis='y')
+        plt.gca().set_xscale('log')
+        plt.legend(bbox_to_anchor=(1,.5))
+        plt.show()
+
+        for i, bin1_key in enumerate(all_bins):
+            for j, bin2_key in enumerate(all_bins):
+                plt.plot(ell, N[:, i, j], 'o:', label='%s-%s'%(bin1_key, bin2_key))
+        plt.xlabel('$\ell$')
+        plt.ylabel('Noise C$_\ell$')
+        plt.gca().ticklabel_format(style='sci',scilimits=(-3,4),axis='y')
+        plt.gca().set_xscale('log')
+        plt.legend(bbox_to_anchor=(1,.5))
+        plt.show()
+
+    ###############################################################################################
+    # (2*ell+1) Tr(S_ell . C_ell^-1 . S_ell . C_ell^-1)
+    C = S+N
+    sn = 0
+    for j in range(len(ell)):
+        inv = np.linalg.inv(C[j, :, :])
+        mat = np.dot(S[j, :, :], inv)
+        mat = np.dot(inv, mat)
+        mat = np.dot(S[j, :, :], mat)
+        sn += (2*ell[j]+1)*np.trace(mat)
+
+    return sn
