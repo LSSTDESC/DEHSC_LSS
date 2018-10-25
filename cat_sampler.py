4c00e8db7ab40c1acda6fc3a1093ec9e4033a47a
David Alonso <dam_phys@gmail.com>
2018-10-25 07:12:51 -0700

    fixed final bugs in COSMOS

--- cat_sampler.py
+++ cat_sampler.py
@@ -25,7 +25,7 @@ parser.add_option('--pz-type',dest='pz_type',default='nnpz',type=str,
                   help='Photo-z to use')
 parser.add_option('--pz-mark',dest='pz_mark',default='best',type=str,
                   help='Photo-z summary statistic to use when binning objects')
-parser.add_option('--use-pdf',dest='usepdf',default=False,
+parser.add_option('--use-pdf',dest='use_pdf',default=False,
                   help='Whether to stack photo-z pdfs to generate N(z)')
 parser.add_option('--use-cosmos',dest='use_cosmos',default=False,
                   help='Whether to use the COSMOS reweighting to generate N(z)')
@@ -47,6 +47,10 @@ parser.add_option('--cosmos-weights-file',dest='cosmos_weights_file',default='/g
 ####
 # Read options
 (o, args) = parser.parse_args()
+
+if o.use_cosmos and o.use_pdf :
+  raise KeyError("Can't do both pdf stacking and COSMOS 30-band")
+  exit(1)
 
 fname_cat=o.prefix_in+'_Catalog_'+o.band+'%.2lf.fits'%o.depth_cut
 if not os.path.isfile(fname_cat) :
@@ -85,7 +89,7 @@ if not o.no_bo_cut :
   msk*=np.logical_not(cat['iflags_pixel_bright_object_any'])
   cat=cat[msk]
 
-if o.usepdf:
+if o.use_pdf:
   # Read in pdfs and bins
   pdf_file = o.prefix_in+'_pdfs_'+o.pz_type+'.fits'
   pdfs = fits.open(pdf_file)[1].data['pdf']
@@ -95,11 +99,6 @@ if o.usepdf:
 if o.use_cosmos:
   # Read in weights and bins
   weights_file = fits.open(o.cosmos_weights_file)[1].data
-  #print(np.amax(weights_file['icmodel_mag']),np.amin(weights_file['gcmodel_mag']))
-  #print(np.amax(cat['icmodel_mag']),np.amin(cat['gcmodel_mag']))
-  #plt.hist(weights_file['icmodel_mag'],range=[17.,25.],bins=50,normed=True)
-  #plt.hist(cat['icmodel_mag'],range=[17.,25.],bins=50,normed=True)
-  #plt.show()
 
 #Read map information
 fsk,mpdum=fm.read_flat_map(o.map_sample,0)
@@ -114,7 +113,7 @@ nzs=[]
 for zi,zf in zip(zi_arr,zf_arr) :
   msk=(cat[column_mark]<=zf) & (cat[column_mark]>zi)
   subcat=cat[msk]
-  if o.usepdf:
+  if o.use_pdf:
     binpdfs = pdfs[msk] # The pdfs which have a redshift in the correct bin
     bz = np.linspace(0,o.nz_bin_max,o.nz_bin_num+1)
     hz = []
@@ -129,12 +128,18 @@ for zi,zf in zip(zi_arr,zf_arr) :
     msk_cosmos=(weights_file['hsc_'+column_mark]<=zf) & (weights_file['hsc_'+column_mark]>zi)
     binweights=weights_file[msk_cosmos]['weight']
     bincosmosz=weights_file[msk_cosmos]['cosmos_photoz']
-    hz,bz=np.histogram(bincosmosz,bins=o.nz_bin_num,range=[0.,o.nz_bin_max],weight=binweights)
+    hz,bz=np.histogram(bincosmosz,bins=o.nz_bin_num,range=[0.,o.nz_bin_max],
+                       weights=binweights)
+    hnz,bnz=np.histogram(bincosmosz,bins=o.nz_bin_num,range=[0.,o.nz_bin_max])
+    ehz=np.zeros(len(hnz)); ehz[hnz>0]=(hz[hnz>0]+0.)/np.sqrt(hnz[hnz>0]+0.)
   else:
     zmcs=subcat[column_zmc]
     hz,bz=np.histogram(zmcs,bins=o.nz_bin_num,range=[0.,o.nz_bin_max])
   nmap=createCountsMap(subcat['ra'],subcat['dec'],fsk)
-  nzs.append([bz[:-1],bz[1:],hz+0.])
+  if o.use_cosmos :
+    nzs.append([bz[:-1],bz[1:],hz+0.,ehz])
+  else :
+    nzs.append([bz[:-1],bz[1:],hz+0.])
   maps.append(nmap)
 nzs=np.array(nzs)
 maps=np.array(maps)
@@ -159,6 +164,9 @@ for im,m in enumerate(maps) :
   cols=[fits.Column(name='z_i',array=nzs[im,0,:],format='E'),
         fits.Column(name='z_f',array=nzs[im,1,:],format='E'),
         fits.Column(name='n_z',array=nzs[im,2,:],format='E')]
+  if o.use_cosmos :
+    cols.append(fits.Column(name='en_z',array=nzs[im,3,:],format='E'))
+    
   hdus.append(fits.BinTableHDU.from_columns(cols))
 hdulist=fits.HDUList(hdus)
 hdulist.writeto(o.fname_out,overwrite=True)
