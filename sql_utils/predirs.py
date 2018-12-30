d6d4ab03cfe999b7b7ec3c05950bbb334d09a92b
David Alonso <dam_phys@gmail.com>
2018-12-30 01:35:05 -0800

    silly one

--- sql_utils/predirs.py
+++ sql_utils/predirs.py
@@ -1,3 +1,3 @@
 predir_saving='/global/cscratch1/sd/damonge/HSC_ceci/'
-arcturus_predir='/global/cscratch1/sd/damonge/HSC/HSC-SSP_brightStarMask_Arcturus'
+arcturus_predir='/global/cscratch1/sd/damonge/HSC_ceci/HSC-SSP_brightStarMask_Arcturus'
 venice_exec=arcturus_predir+'/venice-4.0.3/bin/venice'
