b4996fac94778562dd92595990b04b9d0d5efdbe
David Alonso <dam_phys@gmail.com>
2019-01-06 06:13:14 -0800

    cleanup previous code

--- sample_scripts/sample_nmt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH=/home/lss/local/bin:$PATH
-export LD_LIBRARY_PATH=/home/lss/local/lib:$LD_LIBRARY_PATH
-export MPLBACKEND=PS
-export PYTHONPATH=/home/lss/.local/lib/python2.7/site-packages
-cd /io
-python ./sample_nmt.py
