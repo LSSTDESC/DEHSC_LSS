e16b57bad42b92f981b00cd851ca6ada595d25ff
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2019-07-18 00:38:04 -0700

    Fixed the parts of hsc_lss I broke because I was not able to perform the imports

--- hsc_lss/__init__.py
+++ hsc_lss/__init__.py
@@ -1,6 +1,6 @@
 from ceci import PipelineStage
 from .reduce_cat import ReduceCat
-#from .syst_mapper import SystMapper
+from .syst_mapper import SystMapper
 from .pdf_match import PDFMatch
 from .cosmos_weight import COSMOSWeight
 from .cat_mapper import CatMapper
