bfe88ecb6ef2c77e6a83bb9b5ada3d41082f450b
David Alonso <dam_phys@gmail.com>
2019-08-20 10:16:12 -0700

    corrected for mean

--- plot_scripts/plots_cls_consistency.py
+++ plot_scripts/plots_cls_consistency.py
@@ -32,6 +32,9 @@ for f in field_names:
     s_fields[f]=read_cls(f)
 s_all={typ:sacc.coadd([s_fields[f][typ] for f in field_names],mode='area')
        for typ in ['data','noise','dpj_bias']}
+areas={f:s_fields[f]['data'].meta['Area_rad'] for f in field_names}
+area_total=np.sum(np.array([areas[f]  for f in field_names]))
+weights={f:areas[f]/area_total for f in field_names}
 
 lmaxs=np.array([2170,2515,3185,4017])
 
@@ -44,12 +47,14 @@ for b1 in range(nbins) :
         if b2<b1 :
             axes[b1,b2].axis('off')
 
-
+ndx_all=[]
 for t1,t2,typ,ells,ndx in s_all['data'].sortTracers():
     lmax=min(lmaxs[t1],lmaxs[t2])
     mask=ells<lmax
+    ndx_all+=list(ndx[mask])
     ax=axes[t1,t2]
     dmean=s_all['data'].mean.vector[ndx]-s_all['noise'].mean.vector[ndx]
+    covmean=s_all['data'].precision.getCovarianceMatrix()[ndx,:][:,ndx]
     print("%d-%d"%(t1+1,t2+1))
     ax.plot([2,20000],[0,0],'k--')
     for i_f,f in enumerate(field_names):
@@ -60,7 +65,8 @@ for t1,t2,typ,ells,ndx in s_all['data'].sortTracers():
         e=np.sqrt(np.fabs(np.diag(cov)))
 
         res=(d-dmean)[mask]
-        icov_red=np.linalg.inv(cov[mask,:][:,mask])
+        cov_red=cov-2*weights[f]*cov+covmean
+        icov_red=np.linalg.inv(cov_red[mask,:][:,mask])
         chi2=np.einsum('i,ij,j',res,icov_red,res)
         ndof=len(res)
         print("  %s : %.3lf %d %.3lf"%(f,chi2,ndof,1-chi2f.cdf(chi2,ndof)))
@@ -85,6 +91,21 @@ for t1,t2,typ,ells,ndx in s_all['data'].sortTracers():
     else :
         ax.tick_params(axis='both', which='both',length=0)
         
+print("All:")
+ndx_all=np.array(ndx_all)
+dmean=s_all['data'].mean.vector[ndx_all]-s_all['noise'].mean.vector[ndx_all]
+covmean=s_all['data'].precision.getCovarianceMatrix()[ndx_all,:][:,ndx_all]
+for f in field_names:
+    s=s_fields[f]
+    d=s['data'].mean.vector[ndx_all]-s['noise'].mean.vector[ndx_all]
+    res=d-dmean
+    cov=s['data'].precision.getCovarianceMatrix()[ndx_all,:][:,ndx_all]
+    cov_red=cov-2*weights[f]*cov+covmean
+    icov_red=np.linalg.inv(cov_red)
+    chi2=np.einsum('i,ij,j',res,icov_red,res)
+    ndof=len(res)
+    print("  %s : %.3lf %d %.3lf"%(f,chi2,ndof,1-chi2f.cdf(chi2,ndof)))
+
 fig.legend(loc=(0.15,0.15),frameon=False,ncol=2,fontsize=16)
 plt.savefig('../doc/Paper/figures/cls_consistency.pdf',
             bbox_inches='tight')
