2f0a20148079f7b9a89ec5f97e37f14eea6da10d
David Alonso <dam_phys@gmail.com>
2019-11-18 12:13:23 -0800

    changed masks

--- plot_scripts/plots_masks.py
+++ plot_scripts/plots_masks.py
@@ -1,7 +1,15 @@
 import flatmaps as fm
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy import units as u
 
+yticks={'GAMA09H':[0.,1.,2.],
+        'GAMA15H':[-1., 0., 1.],
+        'HECTOMAP':[43., 44.],
+        'VVDS':[0., 1., 2.],
+        'WIDE12H':[-1., 0., 1.],
+        'XMMLSS':[-6., -5., -4.]
+        }
 for field in ['GAMA09H','GAMA15H','HECTOMAP','VVDS','WIDE12H','XMMLSS']:
     fsk,msk=fm.read_flat_map("/global/cscratch1/sd/damonge/HSC_ceci/WIDE_"+field+"_sirius_i24p5_out/masked_fraction.fits")
     fig=plt.figure()
@@ -11,5 +19,7 @@ for field in ['GAMA09H','GAMA15H','HECTOMAP','VVDS','WIDE12H','XMMLSS']:
                  origin='lower', interpolation='nearest')
     ax.set_xlabel('R.A.', fontsize=14)
     ax.set_ylabel('Dec.', fontsize=14)
+    ax.coords[1].set_ticks(np.array(yticks[field]) * u.deg)
+    ax.coords[1].set_major_formatter('dd')
     plt.savefig("../doc/Paper/figures/mask_"+field+".pdf",bbox_inches='tight')
 plt.show()
