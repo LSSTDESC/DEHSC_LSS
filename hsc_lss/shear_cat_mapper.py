ec77a1f55c7fbaa0ec1d16ec21e36bc4140334fd
Andrina Nicola <aaxnicola@gmail.com>
2020-01-19 12:32:31 -0500

    Added pz estimation to shear cat mapper.

--- hsc_lss/shear_cat_mapper.py
+++ hsc_lss/shear_cat_mapper.py
@@ -3,13 +3,14 @@ from .types import FitsFile,ASCIIFile
 import numpy as np
 from .flatmaps import read_flat_map
 from .map_utils import createSpin2Map
+from .cat_mapper import CatMapper
 from astropy.io import fits
 
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-class ShearCatMapper(PipelineStage) :
+class ShearCatMapper(PipelineStage, CatMapper) :
     name="ShearCatMapper"
     inputs=[('clean_catalog', FitsFile), ('masked_fraction', FitsFile)]
     outputs=[('gamma_maps', FitsFile)]
@@ -64,25 +65,36 @@ class ShearCatMapper(PipelineStage) :
 
         return e2rms_arr
 
-    def parse_input(self):
+    def get_nz_stack(self, cat, codename):
         """
-        Check config parameters for consistency
+        Get N(z) from pdf stacks.
+        :param cat: object catalog
+        :param codename: photoz code name (demp, ephor, ephor_ab, frankenz or nnpz).
         """
-        #Parse input params
-        if self.config['pz_code']=='ephor_ab' :
-            self.pz_code='eab'
-        elif self.config['pz_code']=='frankenz' :
-            self.pz_code='frz'
-        elif self.config['pz_code']=='nnpz' :
-            self.pz_code='nnz'
-        else :
-            raise KeyError("Photo-z method "+self.config['pz_code']+
-                           " unavailable. Choose ephor_ab, frankenz or nnpz")
+        logger.info("Creating pdf stacks for cosmic shear.")
 
-        if self.config['pz_mark']  not in ['best','mean','mode','mc'] :
-            raise KeyError("Photo-z mark "+self.config['pz_mark']+
-                           " unavailable. Choose between best, mean, mode and mc")
-        self.column_mark='pz_'+self.config['pz_mark']+'_'+self.pz_code
+        from scipy.interpolate import interp1d
+
+        f = fits.open(self.pdf_files[codename])
+        p = f[1].data['pdf']
+        z = f[2].data['bins']
+
+        z_all = np.linspace(0., self.config['nz_bin_max'], self.config['nz_bin_num'] + 1)
+        z0 = z_all[:-1]
+        z1 = z_all[1:]
+        zm = 0.5 * (z0 + z1)
+        pzs = []
+        for zi, zf in zip(self.zi_arr, self.zf_arr):
+            msk_bin = (cat[self.column_mark] <= zf) & (cat[self.column_mark] > zi)
+            logger.info("Weighing pdf by WL shape weight.")
+            hz_orig = np.sum(cat['ishape_hsm_regauss_derived_shape_weight'][:, np.newaxis]*p[msk_bin], axis=0)
+            hz_orig /= np.sum(hz_orig)
+            hzf = interp1d(z, hz_orig, bounds_error=False, fill_value=0.)
+            hzm = hzf(zm)
+
+            pzs.append([z0, z1, hzm / np.sum(hzm)])
+
+        return np.array(pzs)
 
     def run(self):
         """
@@ -96,6 +108,24 @@ class ShearCatMapper(PipelineStage) :
 
         logger.info("Reading calibrated shear catalog from {}.".format(self.get_input('clean_catalog')))
         cat = fits.open(self.get_input('clean_catalog'))[1].data
+
+        logger.info("Reading pdf filenames.")
+        data_syst = np.genfromtxt(self.get_input('pdf_matched'),
+                                  dtype=[('pzname', '|U8'), ('fname', '|U256')])
+        self.pdf_files = {n: fn for n, fn in zip(data_syst['pzname'], data_syst['fname'])}
+
+        logger.info("Parsing photo-z bins.")
+        self.zi_arr = self.config['pz_bins'][:-1]
+        self.zf_arr = self.config['pz_bins'][1:]
+        self.nbins = len(self.zi_arr)
+
+        logger.info("Getting COSMOS N(z)s.")
+        pzs_cosmos = self.get_nz_cosmos()
+
+        logger.info("Getting pdf stacks.")
+        pzs_stack = {}
+        for n in self.pdf_files.keys():
+            pzs_stack[n] = self.get_nz_stack(cat, n)
 
         logger.info("Creating shear maps and corresponding masks.")
         gammamaps = self.get_gamma_maps(cat)
@@ -149,6 +179,15 @@ class ShearCatMapper(PipelineStage) :
 
             # e2rms
             cols = [fits.Column(name='e2rms', array=e2rms[im], format='E')]
+            hdus.append(fits.BinTableHDU.from_columns(cols))
+
+            # Nz
+            cols=[fits.Column(name='z_i',array=pzs_cosmos[im,0,:],format='E'),
+                  fits.Column(name='z_f',array=pzs_cosmos[im,1,:],format='E'),
+                  fits.Column(name='nz_cosmos',array=pzs_cosmos[im,2,:],format='E'),
+                  fits.Column(name='enz_cosmos',array=pzs_cosmos[im,3,:],format='E')]
+            for n in self.pdf_files.keys() :
+                cols.append(fits.Column(name='nz_'+n,array=pzs_stack[n][im,2,:],format='E'))
             hdus.append(fits.BinTableHDU.from_columns(cols))
 
         hdulist = fits.HDUList(hdus)
