5f27b799c83962e058b88a1b77fbbad5a8c3bc39
Andrina Nicola <aaxnicola@gmail.com>
2020-02-14 15:45:18 +0000

    Removed double inheritance.

--- hsc_lss/shear_cat_mapper.py
+++ hsc_lss/shear_cat_mapper.py
@@ -10,7 +10,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-class ShearCatMapper(PipelineStage, CatMapper) :
+class ShearCatMapper(CatMapper) :
     name="ShearCatMapper"
     inputs=[('clean_catalog', FitsFile), ('masked_fraction', FitsFile)]
     outputs=[('gamma_maps', FitsFile)]
